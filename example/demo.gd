e4db06ba4ca30522fff15fc4d0710b0f7f89b61a
rothiotome <137085727+rothiotome@users.noreply.github.com>
2025-02-11 14:35:27 +0100

    improved example window

--- example/demo.gd
+++ example/demo.gd
@@ -1,31 +1,54 @@
 extends Node
 
-@onready var login_layout: VBoxContainer = $VBoxContainer/HBoxContainer/LoginLayout
-@onready var logged_in_layout: VBoxContainer = $VBoxContainer/HBoxContainer/LoggedInLayout
-
 func login_anon():
-	VerySimpleTwitch.login_chat_anon(%ChannelName.text)
-	show_logout_layout()
+	await VerySimpleTwitch.login_chat_anon(%ChannelName.text)
+	_show_logout_layout()
 
 func login_token():
-	VerySimpleTwitch.get_token_and_login_chat()
-	show_logout_layout()
+	await VerySimpleTwitch.get_token_and_login_chat()
+	_show_logout_layout()
 
 func logout():
 	VerySimpleTwitch.end_chat_client()
 	%TwitchChat.clear()
-	show_login_layout()
+	_show_login_layout()
 
-func show_login_layout():
-	logged_in_layout.visible = false
-	login_layout.visible = true
+
+#region Local methods to simplify demo
+func _show_login_layout():
+	%TabContainer.set_tab_disabled(0, false)
+	%TabContainer.set_tab_disabled(1, false)
+	%LoggedLayout.hide()
+	%LoginToken.show()
+	%LoginAnon.show()
 	%ChannelName.editable = true
+	%ChannelName.selecting_enabled = true
 
-func show_logout_layout():
-	logged_in_layout.visible = true
-	login_layout.visible = false
+func _show_logout_layout():
+	%TabContainer.set_tab_disabled(0, true)
+	%TabContainer.set_tab_disabled(1, true)
+	%LoginAnon.hide()
+	%LoggedLayout.show()
+	%LoginToken.hide()
 	%ChannelName.editable = false
+	%ChannelName.selecting_enabled = false
 
 
 func clear_chat() -> void:
 	%TwitchChat.clear()
+
+
+func _on_oauth_title_toggle_toggled(toggled_on: bool) -> void:
+	_update_toggle_icon(%OauthTitleToggle, toggled_on)
+
+
+func _on_anonymous_title_toggle_toggled(toggled_on: bool) -> void:
+	_update_toggle_icon(%AnonymousTitleToggle, toggled_on)
+
+
+func _update_toggle_icon(toggle_button: Button, toggled_on: bool) -> void:
+	var icon_path = "res://example/arrow_down.svg" if toggled_on else "res://example/arrow_right.svg"
+	var icon = load(icon_path)
+	toggle_button.icon = icon
+
+#endregion
